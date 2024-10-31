d849d260aa9052cffe38b6740857c02d26b6ebd1
Tyler Boshaw <ty.boshaw@gmail.com>
2024-10-31 00:44:59 -0400

    Updated: Utilizing Lex's C Code to Test

--- rotatingPyramid.cpp
+++ rotatingPyramid.cpp
@@ -1,73 +1,43 @@
-// C++ code for rotating pyramid
-#include <GL/glut.h>
-#include <math.h>
-using namespace std;
-
-float rotationAngle = 0.0f;
-
-void init() {
-    glEnable(GL_DEPTH_TEST); // Enable depth testing
-    glClearColor(0.0f, 0.0f, 0.0f, 1.0f); // Set background color to black
-}
-
-void display() {
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // Clear color and depth buffers
-    glLoadIdentity(); // Reset the model-view matrix
-
-    glTranslatef(0.0f, 0.0f, -7.0f); // Move the pyramid into the scene
-    glRotatef(rotationAngle, 1.0f, 1.0f, 0.0f); // Apply rotation on the X and Y axes
-
-    // Draw pyramid with 4 triangular faces
-    glBegin(GL_TRIANGLES);
-
-    // Front face (Red)
-    glColor3f(1.0f, 0.0f, 0.0f);
-    glVertex3f(0.0f, 1.0f, 0.0f);
-    glVertex3f(-1.0f, -1.0f, 1.0f);
-    glVertex3f(1.0f, -1.0f, 1.0f);
-
-    // Right face (Green)
-    glColor3f(0.0f, 1.0f, 0.0f);
-    glVertex3f(0.0f, 1.0f, 0.0f);
-    glVertex3f(1.0f, -1.0f, 1.0f);
-    glVertex3f(1.0f, -1.0f, -1.0f);
-
-    // Back face (Blue)
-    glColor3f(0.0f, 0.0f, 1.0f);
-    glVertex3f(0.0f, 1.0f, 0.0f);
-    glVertex3f(1.0f, -1.0f, -1.0f);
-    glVertex3f(-1.0f, -1.0f, -1.0f);
-
-    // Left face (Yellow)
-    glColor3f(1.0f, 1.0f, 0.0f);
-    glVertex3f(0.0f, 1.0f, 0.0f);
-    glVertex3f(-1.0f, -1.0f, -1.0f);
-    glVertex3f(-1.0f, -1.0f, 1.0f);
-
-    glEnd();
-
-    glutSwapBuffers(); // Swap the front and back buffers
-}
-
-void update(int value) {
-    rotationAngle += 1.0f; // Increase the rotation angle
-    if (rotationAngle > 360) {
-        rotationAngle -= 360;
+int main() {
+    float A = 0, B = 0;
+    float i, j;
+    int k;
+    float z[1760];
+    char b[1760];
+    printf("\x1b[2J");
+    for (;;) {
+        memset(b, 32, 1760);
+        memset(z, 0, 7040);
+        for (j = 0; j < 6.28; j += 0.07) {
+            for (i = 0; i < 6.28; i += 0.02) {
+                float c = sin(i);
+                float d = cos(j);
+                float e = sin(A);
+                float f = sin(j);
+                float g = cos(A);
+                float h = d + 2;
+                float D = 1 / (c * h * e + f * g + 5);
+                float l = cos(i);
+                float m = cos(B);
+                float n = sin(B);
+                float t = c * h * g - f * e;
+                int x = 40 + 30 * D * (l * h * m - t * n);
+                int y = 12 + 15 * D * (l * h * n + t * m);
+                int o = x + 80 * y;
+                int N = 8 * ((f * e - c * d * g) * m - c * d * e - f * g - l * d * n);
+                if (22 > y && y > 0 && x > 0 && 80 > x && D > z[o]) {
+                    z[o] = D;
+                    b[o] = ".,-~:;=!*#$@"[N > 0 ? N : 0];
+                }
+            }
+        }
+        printf("\x1b[H");
+        for (k = 0; k < 1761; k++) {
+            putchar(k % 80 ? b[k] : 10);
+            A += 0.00004;
+            B += 0.00002;
+        }
+        usleep(30000);
     }
-    glutPostRedisplay(); // Request display update
-    glutTimerFunc(16, update, 0); // 60 FPS (~16 ms per frame)
-}
-
-int main(int argc, char** argv) {
-    glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
-    glutInitWindowSize(800, 600);
-    glutCreateWindow("Rotating 3D Pyramid");
-
-    init();
-    glutDisplayFunc(display);
-    glutTimerFunc(25, update, 0); // 25 ms timer
-    glutMainLoop();
-
     return 0;
 }
