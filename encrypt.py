e4b1153282b20fbb847ddca40d7f551d1ba0c9c7
tsaburro <ty.boshaw@gmail.com>
2024-11-15 18:35:43 -0500

    Updated

--- encrypt.py
+++ encrypt.py
@@ -3,12 +3,19 @@
 def check_login(user, pswd):
     print("TEST LOGIN")
     print("username: " + user)
-    print("password" + pswd)
-    # check database
+    print("password: " + pswd)
+    # check database / decrypt password if it exist
+    # if username and password pair exists, print out msg saying Complete or something
+    # if username and password pair dont exist, print "Username or Password incorrect. Please try again."
 
 def check_login_dupe(user, pswd):
+    value = False
     print("DUPELICATE?")
     # check database
+    # if in database, set value to True
+    # if not, keep as false and send back
+
+    return value
 
 def register():
     print("Create an account")
@@ -16,11 +23,18 @@ def register():
     username = input("username: ")
     password = input("password: ")
     check_login_dupe(username, password)
+    if (check_login_dupe == False):
+        print("Create the account and tell user that account is now created")
+        # encrypt the password in database
+        login()
+    elif (check_login_dupe == True):
+        print("-- Account already exists. Please enter a different username and password. --")
+        register()
 
 def login():
 
-    print("-- A) Login --")
-    print("-- B) Register --")
+    print("-- A Login --")
+    print("-- B Register --")
 
     choice = input(">> ")
 
